8d13ca7aadec6d1d77d1a51c00f121a80bae160f
Connor Kuljis <19459138@student.curtin.edu.au>
2020-08-21 12:51:25 +0800

    W3 DSAStack is done

--- P03/DSAQueue.java
+++ P03/DSAQueue.java
@@ -1,6 +1,3 @@
-/*
- *
- */
 
 import java.util.*;
 
@@ -10,13 +7,75 @@ public class DSAQueue
     private static DEFAULT_CAPACITY = 100;
 
     // class fields
-    private double[] queue;
+    private Object[] queue;
     private int count;
+    private int front;
+    private int rear;
 
     // constructor
-    public DSAQueue(    )
+    public DSAQueue()
     {
-
+        queue = new Object[DEFAULT_CAPACITY];
+        count = 0;
+        front = 0;
+        rear = -1;
     }
+
+    // alternate constructor
+    public DSAQueue(int maxCapacity)
+    {
+        queue = new Object[maxCapacity];
+        count = 0;
+        front = 0;
+        rear = -1;
+    }
+
+    // mutator
+    public void insert(Object inObj)
+    {
+        if (rear == queue.length - 1)
+        {
+            rear = - 1;
+            queue[++rear] = inObj;
+            count++;
+        }
+    }
+
+    // accessor
+    public int getCount()
+    {
+        return count;
+    }
+
+    public boolean isEmpty()
+    {
+        boolean empty = false;
+        if (count == 0)
+        {
+            empty = true;
+        }
+        return empty;
+        // return (count == 0);
+    }
+
+    public boolean isFull()
+    {
+        boolean full = false;
+        if (count == queue.length)
+        {
+            full = true;
+        }
+        return full;
+    }
+
+    // mutator
+    public 
+    public abstract void enqueue (Object inObj);
+
+    public abstract void dequeue (Object inObj);
+
+    public abstract Object peek();
+        
+
 
 }
