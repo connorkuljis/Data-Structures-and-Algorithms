dd7465d6527babfc7d1f7ba8ec4925a9acf4c55e
connorkuljis <conkuljis@gmail.com>
2020-10-18 12:45:39 +0800

    starting json reader

---
+++ Assignment/DSAJson.java
@@ -0,0 +1,19 @@
+/* ***************************************************************************
+ * NAME: DSAJson.java
+ * AUTHOR: Connor Kuljis, 19459138
+ * UNIT: Data Structures and Algorithms (COMP1002)
+ * PURPOSE: 
+ * COMMENT: 
+ * DATE: 2020-10-18
+ * **************************************************************************/
+
+import java.util.*;
+
+public class DSAJson
+{
+    public static void main(String[] args)
+    {
+	System.out.println("hello"); 
+    }
+
+}
---
+++ Assignment/Notes.md
@@ -0,0 +1,4 @@
+# DSA Assignment Notes
+
+# Reminders
+DAIBND, DAIBTC, DAIUSDT, DAIBUSD have 0 trade data and may cause issues when traversing the graph
