acfa1271c8fa7fe1d7985fe9d4871fea0dc4d252
connorkuljis <conkuljis@gmail.com>
2020-10-29 19:05:21 +0800

    CryptoCurrency Asset Objects with fileIO working

--- Assignment/CryptoCurrency.java
+++ Assignment/CryptoCurrency.java
@@ -15,14 +15,14 @@ public class CryptoCurrency
     String symbol;
     double marketCap;
     double price;
-    double circulatingSupply;
+    String circulatingSupply;
     double volume24hr;
     double change1hr;
     double change24hr;
     double change7d;
 
     public CryptoCurrency(String inName, String inSymbol, double inMarketCap,
-	   double inPrice, double inCirculatingSupply, double inVolume24hr,
+	   double inPrice, String inCirculatingSupply, double inVolume24hr,
 	   double inChange1hr, double inChange24hr, double inChange7d)
     {
 	name = inName;
@@ -38,16 +38,17 @@ public class CryptoCurrency
 
     public String toString()
     {
-	return(name + ", " + symbol + ", " + marketCap + ", " + price + ", " +
-	       circulatingSupply + ", " + volume24hr + ", " + change1hr + ", "
-	       + change24hr + ", " + change7d);
+	return("name: " + name + ", symbol:" + symbol + ", market cap: " + marketCap +
+		", price: " + price + ", circulating supply: " + circulatingSupply + 
+		", volume24hr: " + volume24hr + ", change1hr: " + change1hr + ", change24hr: "
+	       + change24hr + ", change7d: " + change7d);
     }
 
     public String getName() { return name; }
     public String getSymbol() { return symbol; }
     public Double getMarketCap() { return marketCap; }
     public Double getPrice() { return price; }
-    public Double getCirculatingSupply() { return circulatingSupply; }
+    public String getCirculatingSupply() { return circulatingSupply; }
     public Double getVolume24hr() { return volume24hr; }
     public Double getChange1hr() { return change1hr; }
     public Double getChange24hr() { return change24hr; }
--- Assignment/FileIO.java
+++ Assignment/FileIO.java
@@ -3,21 +3,15 @@ import java.io.*;
 
 public class FileIO
 {
-    static final String file1 = "pairs.txt";
+    static final String file1 = "asset_info.csv";
 
     public static void main(String[] args)
     {
 	// read the graph from the filenames
-	DSAGraph graph1 = readAL(file1);
+	DSAGraph graph1 = readAsset(file1);
 
-	System.out.println(file1); 
-	// display the first graph
-	graph1.displayAdjacencyList();
-	System.out.print("DFS : "); 
-	displaySearch(graph1.depthFirstSearch());
-	System.out.print("BFS : "); 
-	displaySearch(graph1.breadthFirstSearch());
-	graph1.display();
+	// graph1.displayAdjacencyList();
+	// graph1.display();
 	// end 1
     }
 
@@ -31,7 +25,7 @@ public class FileIO
 
     }
 
-    public static DSAGraph readAL(String inFilename)
+    public static DSAGraph readAsset(String inFilename)
     {
 	DSAGraph theGraph = new DSAGraph();
 
@@ -45,7 +39,10 @@ public class FileIO
 	    rdr = new InputStreamReader(fileStrm);
 	    bufRdr = new BufferedReader(rdr);
 
-	    line = bufRdr.readLine();
+	    line = bufRdr.readLine(); // lets ignore the first line
+	    line = bufRdr.readLine(); // lets ignore the second line
+
+	    line = bufRdr.readLine(); // read the 'first line'
 	    while(line != null)
 	    {
 		processLine(line, theGraph);
@@ -68,13 +65,59 @@ public class FileIO
 	return theGraph;
     }
 
-    public static void processLine(String row, DSAGraph theGraph)
+    private static double regexCurrency(String inStrValue)
     {
-	String[] strArr = row.split(" ");
+	double outVal = 0;
 	try
 	{
-	    String v1 = strArr[0];
-	    String v2 = strArr[1];
+	    outVal = Double.parseDouble(inStrValue.replaceAll("[^\\d.]+", "")); // regex to get only 
+	                                                                        // digits and decimal
+	}
+	catch (Exception e)
+	{
+
+	}
+	return outVal;
+    }
+
+    private static double regexPercentage(String inStrValue)
+    {
+	double outVal = 0;
+	try
+	{
+	    outVal = Double.parseDouble(inStrValue.replaceAll("%", "")); // regex to get only 
+									 // digits, decimal and negative sign
+	}
+	catch (Exception e)
+	{
+
+	}
+	return outVal;
+    }
+
+    // https://stackoverflow.com/questions/1757065/java-splitting-a-comma-separated-string-but-ignoring-commas-in-quotes
+    public static void processLine(String row, DSAGraph theGraph)
+    {
+	String[] strArr = row.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+
+	String name = strArr[1]; 
+	String symbol = strArr[2];
+	double marketCap = regexCurrency(strArr[3]);
+	double price = regexCurrency(strArr[5]);
+	String circulatingSupply = strArr[6];
+	double volume24hr = regexCurrency(strArr[8]);
+	double change1hr = regexPercentage(strArr[9]);
+	double change24hr = regexPercentage(strArr[10]);
+	double change7d = regexPercentage(strArr[11]);
+
+	CryptoCurrency currency = new CryptoCurrency(name, symbol, marketCap, price, 
+		circulatingSupply, volume24hr, change1hr, change24hr, change7d);
+
+	System.out.println(currency); 
+	/*
+	try
+	{
+
 	    if (theGraph.isEmpty())
 	    {
 		theGraph.addVertex(v1, null);
@@ -89,7 +132,7 @@ public class FileIO
 		theGraph.addVertex(v2, null);
 	    }
 
-	    theGraph.addEdge(v1,v2); // finally add the Edge
+	    // theGraph.addEdge(v1,v2); finally add the Edge
 
 	}
 	catch (IllegalArgumentException e)
@@ -97,6 +140,12 @@ public class FileIO
 	    System.out.println("Error trying to process line"); 
 
 	}
+	*/
+    }
+
+    // public static DSAGraph readAsset(String filename)
+    {
+
     }
 
     // public static void writeCSV(DSAQueue queue, String target) throws Exception
